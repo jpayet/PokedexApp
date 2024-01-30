d9a4494a4ad0647c7407a9ad3a7d5dca8e0b884a
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2024-01-30 10:42:48 +0100

    feat: Bottom Tabs Navigation ['Home', 'Search', 'MyTeam', Settings']

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,13 +1,18 @@
 //Libraries
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 //Pages
 import HomeScreen from '../pages/Home';
 import PokemonDetailsScreen from '../pages/PokemonDetails';
+import SearchScreen from '../pages/Search';
+import MyTeamScreen from '../pages/MyTeam';
+import SettingsScreen from '../pages/Settings';
 
 //Variables
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function PokemonListStack() {
     return (
@@ -22,4 +27,55 @@ function PokemonListStack() {
     );
 }
 
-export default PokemonListStack;
+function TabsNavigation() {
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="PokemonListStack" component={PokemonListStack} options={{
+                title: "Pokedex",
+                headerStyle: {
+                    backgroundColor: '#ab0505',
+                },
+                headerTintColor: '#ffffff',
+                headerTitleStyle: {
+                    fontSize: 20,
+                    fontWeight: "bold",
+                }
+            }}/>
+            <Tab.Screen name="SearchScreen" component={SearchScreen} options={{
+                title: "Rechercher",
+                headerStyle: {
+                    backgroundColor: '#ab0505',
+                },
+                headerTintColor: '#ffffff',
+                headerTitleStyle: {
+                    fontSize: 20,
+                    fontWeight: "bold",
+                }
+            }}/>
+            <Tab.Screen name="MyTeamScreen" component={MyTeamScreen} options={{
+                title: "Mon équipe",
+                headerStyle: {
+                    backgroundColor: '#ab0505',
+                },
+                headerTintColor: '#ffffff',
+                headerTitleStyle: {
+                    fontSize: 20,
+                    fontWeight: "bold",
+                }
+            }}/>
+            <Tab.Screen name="SettingsScreen" component={SettingsScreen} options={{
+                title: "Paramètres",
+                headerStyle: {
+                    backgroundColor: '#ab0505',
+                },
+                headerTintColor: '#ffffff',
+                headerTitleStyle: {
+                    fontSize: 20,
+                    fontWeight: "bold",
+                }
+            }}/>
+        </Tab.Navigator>
+    );
+}
+
+export default TabsNavigation;
