e30a7e2512f7239c42a08c329a28090baf462f2d
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2024-03-08 08:23:09 +0100

    feat: [PokemonCard] change component parameters \ refactor: [PokemonCard] stylising

--- components/pokemonCard.js
+++ components/pokemonCard.js
@@ -1,50 +1,68 @@
 //Libraries
 import React, {useEffect, useState} from "react";
-import {View, Image, Text, StyleSheet, TouchableOpacity} from "react-native";
+import {Image, Text, StyleSheet, TouchableOpacity} from "react-native";
 
-export default function PokemonCard({name, url, idPokemon, navigation}) {
+//Libraries propres au projet
+import {capitalize} from "../utils/Routines";
+
+export default function PokemonCard({pokemonData, idPokemon, navigation}) {
     const [uid, setUid] = useState('')
+    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${uid}.png`;
 
     useEffect(() => {
-        if(idPokemon === ""){
-            const uid = url.split('/')[url.split('/').length - 2];
+        if(pokemonData.id === ""){
+            const uid = pokemonData.url.split('/')[pokemonData.url.split('/').length - 2];
             setUid(uid);
         } else{
-            const uid = idPokemon;
+            const uid = pokemonData.id;
             setUid(uid);
         }
     }, []);
 
-    const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${uid}.png`;
 
     return (
         <TouchableOpacity style={styles.card} onPress={() => navigation.navigate('PokemonDetailsScreen', {id:uid})}>
             <Image style={styles.image} source={{uri: image}}/>
-            <View style={styles.titleDiv}>
-                <Text style={styles.title}>{name}</Text>
-            </View>
+            <Text style={styles.phash}>{pokemonData.hash}</Text>
+            <Text style={styles.pname}>{capitalize(pokemonData.name)}</Text>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     card: {
+        width: 185,
+        height: 255,
         margin: 10,
-        borderRadius: 5,
+        padding: 10,
+        paddingTop: 20,
+        borderRadius: 20,
         alignItems: 'center',
-        justifyContent: 'center',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.20,
+        shadowRadius: 1.41,
+        elevation: 2,
+        backgroundColor: '#f2f2f2',
     },
     image: {
-        width: 100,
-        height: 100,
+        width: 150,
+        height: 150,
     },
-    title:{
+    phash:{
+        margin: 10,
+        fontSize: 14,
+        color: 'grey',
+    },
+    pname:{
+        fontSize: 16,
         fontWeight: 'bold',
-        color: 'white',
+        color: 'black',
     },
-    titleDiv: {
-        backgroundColor: '#ab0000',
-        padding: 5,
-        borderRadius: 3,
-    }
+    ptype:{
+        color: 'black',
+    },
 })
--- pages/Home.js
+++ pages/Home.js
@@ -1,25 +1,17 @@
 //Libraries
 import {StatusBar} from 'expo-status-bar';
 import {useEffect, useState} from "react";
-import {FlatList, SafeAreaView, StyleSheet, Text, View, Button} from 'react-native';
+import {FlatList, SafeAreaView, StyleSheet} from 'react-native';
+
+//Libraries propres au projet
+import {formatPokemonId} from "../utils/Routines";
 
 //Composants
 import PokemonCard from "../components/pokemonCard";
 
 export default function Home({navigation}){
-
     const [pokeDatas, setPokeDatas] = useState([]);
     const [nextPage, setNextPage] = useState([]);
-
-    useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon')
-            .then(response => response.json())
-            .then(json => {
-                setNextPage(json.next)
-                setPokeDatas(json.results)
-            })
-            .catch(error => console.error(error));
-    }, []);
 
     const fetchMoreData = () => {
         fetch(nextPage.toString())
@@ -31,12 +23,28 @@ export default function Home({navigation}){
             .catch(error => console.error(error));
     }
 
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon')
+            .then(response => response.json())
+            .then(json => {
+                setNextPage(json.next)
+                setPokeDatas(json.results)
+            })
+            .catch(error => console.error(error));
+    }, []);
+
     return (
         <SafeAreaView style={styles.pokemonsList}>
             <FlatList
-                numColumns={3}
+                numColumns={2}
                 data={pokeDatas}
-                renderItem={({item}) => <PokemonCard name={item.name} url={item.url} idPokemon="" navigation={navigation}/>}
+                renderItem={({item}) => <PokemonCard pokemonData={{
+                    id: item.url.split('/')[item.url.split('/').length - 2],
+                    name: item.name,
+                    url: item.url,
+                    hash: formatPokemonId(item.url)
+                }}
+                navigation={navigation}/>}
                 keyExtractor={item => item.name}
                 onEndReached={fetchMoreData}
                 onEndReachedThreshold={0.5}
