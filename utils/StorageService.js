ea3574b41bff5f8f944f6cdd8ed726840bea7bf6
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2024-03-08 08:26:37 +0100

    feat: New Page MyTeam

---
+++ utils/StorageService.js
@@ -0,0 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const storeData = async (key, value) => {
+    try {
+        await AsyncStorage.setItem(key, value)
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export const getData = async (key) => {
+    try {
+        return await AsyncStorage.getItem(key)
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+export const removeData = async (key) => {
+    try {
+        await AsyncStorage.removeItem(key)
+    } catch(e) {
+        console.error(e);
+    }
+}
